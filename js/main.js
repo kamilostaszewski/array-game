bfed9985da1a7c9ee828f6c3932afd77a2b54385
Kamil Ostaszewski <kamisostaszewski@gmail.com>
2022-09-28 17:54:54 +0200

    create board and append to #app

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
 let array = [];
 let app = document.getElementById('app');
+let board = document.createElement('div');
 
+function createBoard(board, array) {
+    board.classList.add('board');
+    
+    for (let i = 0; i < 100; i++) {
+        array.push(i);
+    }
+    
+    array.forEach(
+        function(el, index) {
+            let div = document.createElement('div');
+            div.dataset.index = index;
+            if (index === 65) {
+                div.classList.add('player');
+            }
+            board.appendChild(div);
+        }
+    );
 
-for (var i = 0; i < 100; i++) {
-    array.push(i);
+    return [board, array]
 }
 
-array.forEach(function(element, index) {
-    let div = document.createElement('div');
-    div.classList.add(index);
+[board, array] = createBoard(board, array);
 
-    app.appendChild(div);
-});
-
-console.log(array,app   )
+app.appendChild(board);
